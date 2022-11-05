c4e4a20f679ac4b9d1c590a6c29668c1048aee6d
Imie Nazwisko <me@imienazwisko.com>
2022-11-05 20:27:58 +0100

    zadania

--- 04-Subroutines/zadanie11.py
+++ 04-Subroutines/zadanie11.py
@@ -1,12 +1,7 @@
 def factorial(n):
-
-    # 0! = 1, 1! = 1
     if n==0 or n==1:
         return 1
-
-    # n! = n * (n-1)!
-    if n > 1:
+    if n > 1:                           # n! = n * (n-1)!
         return n * factorial(n-1)
-  
 x = 5
 print( f"{x}! = {factorial(x)}" )
